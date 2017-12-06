aa21eb566ddff560b3b59f79565aa3ec7673b340
connor williams <herefordbulls44@gmail.com>
2017-12-06 08:48:00 -0700

    connor

---
+++ Intex2/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Intex2.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int empID { get; set; }
+        public string empFirstName { get; set; }
+        public string empLastName { get; set; }
+        public string empSSNHas { get; set; }
+        public decimal empWage { get; set; }
+        public string empEmail { get; set; }
+    }
+}
